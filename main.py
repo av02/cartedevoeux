e6b19c22de9f4304087f8aabe7ea8f0cc4db2592
av02 <39122034+av02@users.noreply.github.com>
2020-01-14 00:31:06 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,45 @@
+#importation des formes et du module tkinter
+#
+import formes.etoile as et
+import formes.Fond as fd
+import tkinter
+import time
+import formes.formes as f
+import texte.nom as nom
+
+#creation d'une fenetre
+fenetre_principale = tkinter.Tk()
+fenetre_principale.title("bonne année")
+fenetre_principale.geometry("350x350+700+300")
+fenetre_principale.configure(bg="#777777")
+
+test = nom.nom()
+test.demander_nom(fenetre_principale)
+
+
+#definition du cadre de dessin de l'etoile
+canva = tkinter.Canvas(fenetre_principale,height=350,width=350)
+canva.pack()
+
+
+#definition d'une étoile
+premiere_etoile = f.formes(12,"#FFD700",80,80,"etoile")
+#definition du fond
+fond = fd.fond("#ff2700","#c6c318","#a1b9fb",canva)
+fond.tracer_fond()
+#dessin de l'étoile
+fond.tracer_fond()
+fond.clignoter(fenetre_principale,120,[["#ff2700","#c6c318","#a1b9fb"],["pink","yellow","green"]])
+premiere_etoile.tracer(canva)
+
+
+etoile2 = f.formes(24,"#AA45AA",270,270,"etoile")
+
+sapin1 = f.formes(150,"black",120,300,"sapin","green")
+sapin1.tracer(canva)
+etoile2.tracer(canva)
+
+premiere_etoile.clignoter(fenetre_principale,200,["#AA00AA","#FFD700"])
+etoile2.clignoter(fenetre_principale,200,["pink","yellow","green"])
+
+fenetre_principale.mainloop()
