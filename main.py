6474012f307547b6c834a3534a8e338faffc5c5c
av02 <39122034+av02@users.noreply.github.com>
2020-01-19 23:39:11 +0100

    Update main.py
    ajout d'appel aux nouvelles fonctions

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 #importation des formes et du module tkinter
-#
 import formes.etoile as et
 import formes.Fond as fd
 import tkinter
@@ -7,14 +6,21 @@ import time
 import formes.formes as f
 import texte.nom as nom
 
+fenetre_start = tkinter.Tk()
+fenetre_start.title("bonne année")
+fenetre_start.geometry("350x350+700+300")
+fenetre_start.configure(bg="#777777")
+
+a = nom.ecran_base(fenetre_start)
+fenetre_start.wait_window()
+prenom =a.get_nom()
+
+
 #creation d'une fenetre
 fenetre_principale = tkinter.Tk()
 fenetre_principale.title("bonne année")
 fenetre_principale.geometry("350x350+700+300")
 fenetre_principale.configure(bg="#777777")
-
-test = nom.nom()
-test.demander_nom(fenetre_principale)
 
 
 #definition du cadre de dessin de l'etoile
@@ -28,9 +34,10 @@ premiere_etoile = f.formes(12,"#FFD700",80,80,"etoile")
 fond = fd.fond("#ff2700","#c6c318","#a1b9fb",canva)
 fond.tracer_fond()
 #dessin de l'étoile
-fond.tracer_fond()
+
 fond.clignoter(fenetre_principale,120,[["#ff2700","#c6c318","#a1b9fb"],["pink","yellow","green"]])
 premiere_etoile.tracer(canva)
+
 
 
 etoile2 = f.formes(24,"#AA45AA",270,270,"etoile")
@@ -41,5 +48,9 @@ etoile2.tracer(canva)
 
 premiere_etoile.clignoter(fenetre_principale,200,["#AA00AA","#FFD700"])
 etoile2.clignoter(fenetre_principale,200,["pink","yellow","green"])
+
+text = canva.create_text(220,110,text = "bonne année "+prenom)
+
+
 
 fenetre_principale.mainloop()
