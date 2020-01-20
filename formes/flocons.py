dbd5e69b1b819de878c21666e0af76904012ceb3
av02 <39122034+av02@users.noreply.github.com>
2020-01-20 22:23:09 +0100

    Add files via upload
    ajout de la surcouche flocons

---
+++ formes/flocons.py
@@ -0,0 +1,43 @@
+import tkinter
+import random
+class flocons:
+    """
+    canva : un canva tkinter
+    permet de generer un flocon aleatoire
+    """
+    def __init__(self,canva):
+        self.canva = canva
+        self.largeur=random.randint(1,5)
+        self.x = random.randint(0,350)
+        self.y = 0
+        self.id = self._flocon(self.x,self.y,self.largeur)
+
+   
+    def _flocon(self, posx,posy,largeur):
+        id = self.canva.create_oval(posx,posy,posx+largeur,posy+largeur,width=0,fill="#FFFAFA")
+        return id
+
+    def deplacer(self):
+        y = random.randint(0,5)
+        self.y+=y
+        x = random.randint(-1,1)
+        self.x = x
+        self.canva.move(self.id,x,y)
+
+
+
+tous_flocons = []
+
+def mouvement(canva):
+    for i in range(0,6):
+        tous_flocons.append(flocons(canva))
+    for flocon in tous_flocons:
+        if flocon.y > 350:
+            del flocon
+
+        else:
+            flocon.deplacer()
+            pass
+    canva.after(10,mouvement, canva)
+
+
