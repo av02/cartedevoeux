77c646601c0547fedf7281203fb4e55665d56dac
av02 <39122034+av02@users.noreply.github.com>
2020-01-20 22:52:19 +0100

    Update main.py
    ajout et reorganisation des exemples

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+"""
+un exemple simple avec toutes les fonctions disponible
+
+"""
 #importation des formes et du module tkinter
 import formes.etoile as et
 import formes.Fond as fd
@@ -5,7 +9,10 @@ import tkinter
 import time
 import formes.formes as f
 import texte.nom as nom
+import formes.flocon as flo
 
+#une premiere fenetre pour demander le prenom
+#TODO: ajouter un fond d'ecran
 fenetre_start = tkinter.Tk()
 fenetre_start.title("bonne année")
 fenetre_start.geometry("350x350+700+300")
@@ -23,34 +30,37 @@ fenetre_principale.geometry("350x350+700+300")
 fenetre_principale.configure(bg="#777777")
 
 
-#definition du cadre de dessin de l'etoile
+#definition du cadre de dessin 
 canva = tkinter.Canvas(fenetre_principale,height=350,width=350)
 canva.pack()
 
 
-#definition d'une étoile
-premiere_etoile = f.formes(12,"#FFD700",80,80,"etoile")
 #definition du fond
 fond = fd.fond("#ff2700","#c6c318","#a1b9fb",canva)
-fond.tracer_fond()
-#dessin de l'étoile
+fond.tracer_fond()#dessiner le fond
+fond.clignoter(fenetre_principale,120,[["#ff2700","#c6c318","#a1b9fb"],["pink","yellow","green"]])#faire clignoter
 
-fond.clignoter(fenetre_principale,120,[["#ff2700","#c6c318","#a1b9fb"],["pink","yellow","green"]])
+
+#definition d'une étoile puis tracé puis clignoter d'une etoile
+premiere_etoile = f.formes(12,"#FFD700",80,80,"etoile")
 premiere_etoile.tracer(canva)
-
-
-
-etoile2 = f.formes(24,"#AA45AA",270,270,"etoile")
-
-sapin1 = f.formes(150,"black",120,300,"sapin","green")
-sapin1.tracer(canva)
-etoile2.tracer(canva)
-
 premiere_etoile.clignoter(fenetre_principale,200,["#AA00AA","#FFD700"])
+
+
+#une deuxieme etoile
+etoile2 = f.formes(24,"#AA45AA",270,270,"etoile")
+etoile2.tracer(canva)
 etoile2.clignoter(fenetre_principale,200,["pink","yellow","green"])
 
+#un sapin
+sapin1 = f.formes(150,"black",120,300,"sapin","green")
+sapin1.tracer(canva)
+
+#le texte
+#TODO: modifier la police/taille
 text = canva.create_text(220,110,text = "bonne année "+prenom)
 
-
+#faire neiger
+flo.surcouche_neige(canva)
 
 fenetre_principale.mainloop()
